22c2c5241d4e1c7a399b4ca1a0a9593f73a2be68
User <user@example.com>
2026-05-22 19:16:44 +0530

    Allow localhost and testserver in ALLOWED_HOSTS

--- food_delivery/food_delivery/settings.py
+++ food_delivery/food_delivery/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-^xbl*h*tk$p*v1gcoj5=8h=h4v0%e6-1*o+zt+3wxn&kh%j(*^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '[::1]',
+    'testserver',
+]
 
 
 # Application definition
