72ad4ea92fd32e94e4700b68585751308f1ccb39
Ankita Pati <ankitapati2615@gmail.com>
2026-07-07 13:20:28 +0530

    Add Django signals for automatic cart creation

---
+++ food_delivery/accounts/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from accounts.models import User
+from cart.models import Cart
+
+
+@receiver(post_save, sender=User)
+def create_cart(sender, instance, created, **kwargs):
+
+    if created:
+
+        print("SIGNAL WORKED")
+
+        Cart.objects.create(user=instance)
